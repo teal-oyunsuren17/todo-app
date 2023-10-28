379977a57d4856a934c6843b7ea84646f773e71e
Oyunsuren99 <p.oyunsuren0129@gmailcom>
2023-10-28 15:54:05 +0800

    create next and move raect components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,183 @@
-import Image from 'next/image'
+"use client"
+import React, { useEffect, useState } from "react"
+import { DragDropContext, Droppable } from "react-beautiful-dnd"
+import axios from "axios"
+import { CreateTasks } from "@/utils/CreateTask"
+import TaskCard from "@/utils/TaskCard"
+import styled from "@emotion/styled"
+import { toast, Toaster } from "react-hot-toast"
 
-export default function Home() {
+const Container = styled.div`
+  display: flex;
+`
+
+const TaskList = styled.div`
+  min-height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f3f3f3;
+  min-width: 40%;
+  border-radius: 5px;
+  padding: 3% 3%;
+`
+
+const TaskColumnStyles = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5%;
+  justify-content: center;
+  width: 100%;
+  margin: 0 auto;
+`
+
+const Title = styled.span`
+  color: #4e65ff;
+  // background: rgba(147, 197, 253, 0.3);
+  border-radius: 5px;
+  text-align: center;
+  margin-bottom: 10px;
+  width: 100%;
+`
+
+const Kanban = () => {
+  const [datas, setDatas]: any = React.useState([])
+  const [Todos, setTodos] = React.useState([])
+  const [Dones, setDones] = React.useState([])
+  const [columns, setColumns] = React.useState({})
+
+  const notify = () => toast("Here is your toast.")
+
+  function getDatas() {
+    axios.get(`https://todo-back-q1ut.onrender.com/list`).then((res) => {
+      if (res.status === 200) {
+        setDatas(res.data)
+      } else {
+        alert(`data fetch error ${res.status}`)
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    getDatas()
+  }, [])
+
+  React.useEffect(() => {
+    const fTodos = datas.filter((data: any) => data.done === false)
+    const fDones = datas.filter((data: any) => data.done === true)
+
+    setTodos(fTodos)
+    setDones(fDones)
+  }, [datas])
+
+  const columnsFromBackend = {
+    123: {
+      title: "To-Do",
+      items: Todos,
+    },
+    456: {
+      title: "Done",
+      items: Dones,
+    },
+  }
+
+  React.useEffect(() => {
+    setColumns(columnsFromBackend)
+  }, [Todos, Dones])
+
+  const onDragEnd = (result: any, columns: any, setColumns: any) => {
+    if (!result.destination) return
+    const { source, destination } = result
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId]
+      const destColumn = columns[destination.droppableId]
+      const sourceItems = [...sourceColumn.items]
+      const destItems = [...destColumn.items]
+      const [removed] = sourceItems.splice(source.index, 1)
+      destItems.splice(destination.index, 0, removed)
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      })
+      axios
+        .put(`https://todo-back-q1ut.onrender.com/list/${result.draggableId}`, {
+          done: destination.droppableId === "123" ? false : true,
+        })
+
+        .then(function (response) {
+          if (response.status === 200) {
+            notify()
+            getDatas()
+          }
+        })
+        .catch(function (error) {
+          // console.log(error)
+        })
+    } else {
+      const column = columns[source.droppableId]
+      const copiedItems = [...column.items]
+      const [removed] = copiedItems.splice(source.index, 1)
+      copiedItems.splice(destination.index, 0, removed)
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      })
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div
+      style={{
+        backgroundImage: "linear-gradient(100deg, #575656, #062e3f)",
+        height: "100vh",
+        padding: "10px 1%",
+      }}
+    >
+      <Toaster />
+      <CreateTasks getDatas={getDatas} />
+      <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
+        <Container>
+          <TaskColumnStyles>
+            {Object.entries(columns).map(([columnId, column]: any, index) => {
+              return (
+                <Droppable key={columnId} droppableId={columnId}>
+                  {(provided, snapshot) => (
+                    <TaskList
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <Title>{column.title}</Title>
+                      {column.items.map((item: any, index: number) => (
+                        <TaskCard
+                          key={item._id}
+                          item={item}
+                          index={index}
+                          getDatas={getDatas}
+                        />
+                      ))}
+                      {provided.placeholder}
+                    </TaskList>
+                  )}
+                </Droppable>
+              )
+            })}
+          </TaskColumnStyles>
+        </Container>
+      </DragDropContext>
+    </div>
   )
 }
+
+export default Kanban
